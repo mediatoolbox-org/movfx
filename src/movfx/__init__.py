df4e11289cafa5d39fb7fa7e1990fd5be9cc4ef3
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2026-03-01 15:38:46 +0000

    fix: fix versioning

--- src/movfx/__init__.py
+++ src/movfx/__init__.py
@@ -1,6 +1,16 @@
 """MovFX: Image transition effects with sound."""
 
+from importlib import metadata as importlib_metadata
 
-def __version__() -> str:
-    """Return the package version."""
-    return "0.1.0"  # semantic-release
+
+def get_version() -> str:
+    """Return the program version."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.1.0"  # semantic-release
+
+
+__author__ = """Ivan Ogasawara"""
+__email__ = "ivan.ogasawara@gmail.com"
+__version__: str = get_version()
---
+++ src/movfx/core.py
@@ -0,0 +1,1 @@
+"""MovFX core module."""
